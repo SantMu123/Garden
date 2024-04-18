88194a18dbed8144b02f7bce0425d768a97b2a7c
SantMu123 <126543033+SantMu123@users.noreply.github.com>
2024-04-18 17:32:42 -0500

    feat: :sparkles: Ejercicio de multiplicar por dos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import prompt from 'async-prompt'
 import {sumarRango} from "./modulos/sumarRangoNumero.js";
+import {multiplicarPorDos} from "./modulos/multiplicarPorDos.js";
 
 //let input = prompt();
+/*
 let num1 = Number(await prompt("ingrese el numero 1: "))
 let num2 = Number(await prompt("ingrese el numero 2: "))
 console.log(sumarRango(num1, num2));
+*/
 //console.log(sumarRango(1,10));
+console.log(multiplicarPorDos([1, 2, 3]))
---
+++ js/modulos/multiplicarPorDos.js
@@ -0,0 +1,4 @@
+export const multiplicarPorDos = function duplicar(arr){
+    let nuevo = arr.map(num => num*2);
+    return nuevo
+}
